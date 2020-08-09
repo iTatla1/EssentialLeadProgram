c8792b958659555a3b46e118b9edf952e00a7784
Muhammad Usman <usm.gh36@gmail.com>
2020-08-09 21:50:10 +0500

    Deliver Invalid Data errors

--- EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol HTTPClient{
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteFeedLoader{
@@ -17,6 +17,7 @@ public final class RemoteFeedLoader{
     private let client: HTTPClient
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     public init(url: URL, client: HTTPClient) {
@@ -25,7 +26,14 @@ public final class RemoteFeedLoader{
     }
     
     public func load(completion: @escaping (RemoteFeedLoader.Error) -> Void){
-        client.get(from: url){_ in completion(.connectivity)}
+        client.get(from: url){error,response  in
+            if response != nil {
+                completion(.invalidData)
+            }
+            else{
+                completion(.connectivity)
+            }
+        }
         
     }
 }
