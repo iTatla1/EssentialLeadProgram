5aec75a7d08fd1e24d65727bdb99cf31e5e47933
Muhammad Usman <usm.gh36@gmail.com>
2020-08-10 21:30:37 +0500

    Deliver item response on 200HTTP Response with items

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -68,6 +68,38 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliversItemsOn200HTTPResponseWithJsonItems(){
+        let (sut, client) = makeSUT()
+        
+        let item1 = FeedItem(id: UUID(), description: nil, location: nil, imageURL: URL(string: "http://www.a-utl.com")!)
+       
+        let item2 = FeedItem(id: UUID(), description: "A description", location:
+            "A location", imageURL: URL(string: "http://www.another-utl.com")!)
+              
+        
+        let item1JSON = [
+            "id": item1.id.uuidString,
+            "image": item1.imageURL.absoluteString,
+        ]
+        
+        let item2JSON = [
+            "id": item2.id.uuidString,
+            "image": item2.imageURL.absoluteString,
+            "description": item2.description,
+            "location": item2.location,
+        ]
+        
+        let itemsJSON = [
+            "items": [item1JSON, item2JSON]
+        ]
+        
+        expect(sut, toCompleteWithResult: .success([item1,item2]), when: {
+            let jsonData: Data = try! JSONSerialization.data(withJSONObject: itemsJSON)
+            client.complete(withStatusCode: 200, data: jsonData)
+        })
+        
+    }
+    
     
     // MARK:- Test Helper
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
