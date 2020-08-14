ccbe5c6915e5b16bca6fa7696f62b11f3fafeaff
Muhammad Usman <usm.gh36@gmail.com>
2020-08-14 12:34:11 +0500

    Resumes data tasks on creation

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -16,7 +16,7 @@ class URLSessionHTTPClient{
     }
     
     func get(from url: URL){
-        session.dataTask(with: url, completionHandler: {_,_,_ in})
+        session.dataTask(with: url, completionHandler: {_,_,_ in}).resume()
     }
 }
 
@@ -31,19 +31,45 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLS, [url])
     }
     
+    func test_getFromURL_resumesDataTaskFromURL(){
+        let url = URL(string: "http://www.a-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     
     //MARK:- Helper
     private class URLSessionSpy: URLSession {
         var receivedURLS: [URL] = []
+        var stubs: [URL: URLSessionDataTask] = [:]
+        
+        func stub(url: URL, task: URLSessionDataTask){
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLS.append(url)
             
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
         
     }
     
-    private class FakeURLSessionDataTask: URLSessionDataTask{}
+    private class FakeURLSessionDataTask: URLSessionDataTask{
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask{
+        var resumeCallCount: Int = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
 }
 
