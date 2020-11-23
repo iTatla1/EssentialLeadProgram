61d083dfb28708e444b4998f14c2ac96d380ace7
Muhammad Usman <usm.gh36@gmail.com>
2020-11-23 11:11:43 +0500

    validate cache command doesn't delete less than seven days old cache

--- EssentialFeed/EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -27,12 +27,24 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
-    func test_load_doesNotDeletesCacheOnEmptyCache() {
+    func test_validateCache_doesNotDeletesCacheOnEmptyCache() {
         let (sut, store) = makeSUT()
         
         sut.validateCache()
         
         store.completeRetrievalWithEmptyCache()
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_validateCache_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: {fixedCurrentDate})
+        
+        sut.validateCache()
+        
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
@@ -48,5 +60,29 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
     
     private func anyNSError() -> NSError {
         return NSError(domain: "any-error", code: 33, userInfo: nil)
+    }
+    
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let model = [uniqueImage(), uniqueImage(), uniqueImage()]
+        let local = model.map{LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
+        return (model,local)
+    }
+    
+    private func anyURL() -> URL{
+        return URL(string: "http://www.a-url.com")!
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
