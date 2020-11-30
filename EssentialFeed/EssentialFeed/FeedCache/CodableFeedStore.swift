601179e92e57dd6b6b168dc5c2821845bc639072
Muhammad Usman <usm.gh36@gmail.com>
2020-11-30 08:02:58 +0500

    Make CodableFeedStore Queue to run retrieve process concurrently and added barriers for other operations to avoid race-conditions

--- EssentialFeed/EssentialFeed/FeedCache/CodableFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/CodableFeedStore.swift
@@ -11,7 +11,7 @@ import Foundation
 public class CodableFeedStore: FeedStore {
     private let storeURL: URL
     
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
     
     public init(storeURL: URL) {
         self.storeURL = storeURL
@@ -63,7 +63,7 @@ public class CodableFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timeStamp: Date, completion: @escaping InsertCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             do {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timeStamp: timeStamp)
@@ -79,7 +79,7 @@ public class CodableFeedStore: FeedStore {
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else { return completion(nil)}
             do {
                 try FileManager.default.removeItem(at: storeURL)
