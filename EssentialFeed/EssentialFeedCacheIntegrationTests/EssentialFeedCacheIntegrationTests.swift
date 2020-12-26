196eecafdfed9285456543c2c2256019a4df66f3
Muhammad Usman <usm.gh36@gmail.com>
2020-12-26 09:20:52 +0500

    add essentialfeedcacheintegrationtest to separate potential slower integration test to faster unit tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Muhammad Usman Tatla on 26/12/2020.
+//  Copyright © 2020 Muhammad Usman Tatla. All rights reserved.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+
+}
