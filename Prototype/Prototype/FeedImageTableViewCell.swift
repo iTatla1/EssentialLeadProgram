c5a083a07dba4373df3bc0babd5733ca2291aa8a
Muhammad Usman <usm.gh36@gmail.com>
2021-01-02 10:55:50 +0500

    Added Feed Image Cell with storyboard connection

---
+++ Prototype/Prototype/FeedImageTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageTableViewCell.swift
+//  Prototype
+//
+//  Created by Muhammad Usman Tatla on 02/01/2021.
+//
+
+import UIKit
+
+class FeedImageTableViewCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIStackView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    
+    
+}
