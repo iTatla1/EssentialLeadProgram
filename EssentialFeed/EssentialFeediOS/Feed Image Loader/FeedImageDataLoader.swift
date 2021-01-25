9709cea4aaf25380b43f65f9fa07b318995e01d7
Muhammad Usman <usm.gh36@gmail.com>
2021-01-25 09:03:02 +0500

    Extrtacted UIRefreshcontroller implementation and configuration and separated it into its own file FeedViewRefreshController

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Muhammad Usman Tatla on 25/01/2021.
+//  Copyright © 2021 Muhammad Usman Tatla. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
