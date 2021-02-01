f57df05a73181cc594ff0622b7190a9afa3c77a3
Muhammad Usman <usm.gh36@gmail.com>
2021-02-01 12:35:14 +0500

    Move title string creation from FeedViewController ro FeedPresenter in MVP, presentation data should be created in Presenter

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -27,6 +27,10 @@ protocol FeedView {
 final class FeedPresenter {
     typealias Observer<T> = (T) -> Void
     
+    static var title: String {
+        "My Feed"
+    }
+    
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
     
